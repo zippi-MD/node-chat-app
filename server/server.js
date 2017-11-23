a006e8200bda692cef3168264392875fc68d701a
Alejandro Mendoza <zippi180@gmail.com>
2017-11-22 23:32:32 -0600

    Added generateMessage Utility and test.

--- server/server.js
+++ server/server.js
@@ -10,32 +10,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const {generateMessage} = require('./utils/message')
+
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from: "Admin",
-        text: "Welcome to the chat app",
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App.'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
 
 
     socket.on('createMessage', (newMessage) => {
         console.log('Message', newMessage);
 
-        io.emit('newMessage', {
-            from: newMessage.from,
-            text: newMessage.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
         // socket.broadcast.emit('newMessage', {
         //     from: newMessage.from,
         //     text: newMessage.text,
