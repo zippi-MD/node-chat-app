8a82c9d6d15225148f71206758f404ff06a0efbc
Alejandro Mendoza <zippi180@gmail.com>
2017-11-22 23:00:58 -0600

    Greet new user and alert others.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
 
     socket.on('createMessage', (newMessage) => {
         console.log('Message', newMessage);
@@ -23,8 +35,12 @@ io.on('connection', (socket) => {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
-        })
-        ;
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
