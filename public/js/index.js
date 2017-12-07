20bfdeee7efb7229b33e5b569f679f39c27d6508
Alejandro Mendoza <zippi180@gmail.com>
2017-12-06 20:55:00 -0600

    Added mustache js for template messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,43 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-   let formattedTime = moment(message.createdAt).format('h:mm a');
-   let li = jQuery('<li></li>');
-   li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-   jQuery('#messages').append(li);
+    let formattedTime = moment(message.createdAt).format('h:mm a');
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
+
+
+   // let li = jQuery('<li></li>');
+   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+   //
+   // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
+    let template = jQuery('#location-message-template').html();
+    let html = Mustache.render(template, {
+       from: message.from,
+        createdAt: formattedTime,
+        url: message.url
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
+    // let li = jQuery('<li></li>');
+    // let a = jQuery('<a target="_blank">My current location</a>');
+    //
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    //
+    // jQuery('#messages').append(li);
 });
 
 
